3d4ec74cd8731f1820a8db70b3e23426ac5a3095
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-17 16:40:21 -0500

    add evn port

--- devServer.js
+++ devServer.js
@@ -17,7 +17,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.listen(3000, 'localhost', (err) => {
+app.listen(process.env.PORT || 3000, 'localhost', (err) => {
   if (err) {
     console.log(err)
     return
