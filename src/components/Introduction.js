d51c6da9c9fa44b375408b73d9dfeb4228ae1312
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-18 15:39:17 -0500

    add  img circle

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,13 +4,17 @@ export default class Introduction extends Component {
    render() {
       const {personalInfo} = this.props
       return (
-        <div className="container">
+         <div className="container">
             <div className="row">
-                <div className="col-sm-offset-1 col-sm-10">
-                    <h3>{personalInfo.bio}</h3>
-                </div>
+               <div className="col-sm-offset-1 col-sm-10">
+
+                  <h3 className="bio">
+                    <img className="img-circle" src="https://avatars3.githubusercontent.com/u/9455409?v=3&s=466" ALIGN="left" height="200" width="200"/>
+                    {personalInfo.bio}
+                  </h3>
+               </div>
             </div>
-        </div>
+         </div>
       )
    }
 }
