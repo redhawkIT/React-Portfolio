7c769f5ac5b7200e8b5bfa25b64fe6714e78dc19
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-18 16:31:37 -0500

    update bio

--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -3,7 +3,7 @@ const personalInfoData = {
    linkedIn: 'https://www.linkedin.com/in/christopherwiles512',
    github: 'https://github.com/ChrisWiles',
    angelList: 'https://angel.co/',
-   bio: 'I’m a user-focused, full stack software engineer with a strong background and expertise in JavaScript. What I love about software engineering is the potential to develop clean, scalable applications that are a joy for users and programmers alike.',
+   bio: 'My name is Christopher Wiles and I am a full-stack Web Application Developer and , currently living in Austin, TX. I spend my whole day, practically every day, experimenting with HTML, CSS, and JavaScript; along with inhaling a wide variety of potentially useless information through a few hundred RSS feeds. I am both driven and self-motivated, and I am constantly experimenting with new technologies and techniques. I am very passionate about Web Development, and strive to better myself as a developer, and the development community as a whole.',
    email: 'mailto:chriswiles87@gmail.com',
    resume: 'https://drive.google.com/open?id=1wJ_zFvz669sdqUCjNdELGPNjHX4pZ_NMPIEuYgZ1mok'
 }
