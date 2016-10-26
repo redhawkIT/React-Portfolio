06d66f27859e5ccfc42f010399aca98d9be1b768
redhawkIT <khodkodmitry10011983@hotmail.com>
2016-10-26 16:17:20 -0500

    refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,40 +2,42 @@ import React, { Component } from 'react'
 
 export default class Header extends Component {
   render() {
-    const { personalInfo } = this.props
+    const { name, resume, email, github, linkedIn, angelList } = this.props.personalInfo
     return (
           <nav className="navbar navbar-fixed-top navbar-inner header">
 
             <div className="container">
               <div className="navbar-brand">
-                <a href="#" > {personalInfo.name} </a>
+                <a href="#" > {name} </a>
               </div>
               <ul className="nav navbar-nav pull-right">
                 <li>
-                  <a href={personalInfo.resume} className="nav-link" target="_blank" style={{ lineHeight: 'normal' }}>
+                  <a href={resume} className="nav-link" target="_blank" style={{ lineHeight: 'normal' }}>
                     <span>Resume</span>
                   </a>
                 </li>
                 <li>
-                  <a href={personalInfo.email} className="nav-link">
+                  <a href={email} className="nav-link">
                     <i className="fa fa-envelope icon"></i>
                   </a>
                 </li>
                 <li>
-                  <a href={personalInfo.github} className="nav-link" target="_blank">
+                  <a href={github} className="nav-link" target="_blank">
                     <i className="fa fa-github icon"></i>
                   </a>
                 </li>
                 <li>
-                  <a href={personalInfo.linkedIn} className="nav-link" target="_blank">
+                  <a href={linkedIn} className="nav-link" target="_blank">
                     <i className="fa fa-linkedin-square icon"></i>
                   </a>
                 </li>
-                <li>
-                  <a href={personalInfo.angelList} className="nav-link" target="_blank">
-                    <i className="fa fa-angellist icon"></i>
-                  </a>
-                </li>
+                {angelList &&
+                  <li>
+                    <a href={angelList} className="nav-link" target="_blank">
+                      <i className="fa fa-angellist icon"></i>
+                    </a>
+                  </li>
+                }
                 </ul>
             </div>
             </nav>
--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -2,7 +2,7 @@ const personalInfoData = {
    name: 'Christopher Wiles',
    linkedIn: 'https://www.linkedin.com/in/christopherwiles512',
    github: 'https://github.com/ChrisWiles',
-   angelList: 'https://angel.co/',
+   angelList: null,
    bio: 'My name is Christopher Wiles, and I am a full-stack Web Application Developer currently living in Austin, TX. I spend my whole day, practically every day, experimenting with the JavaScript ecosystem and inhaling a wide variety of potentially useless information through a few hundred RSS feeds. I am driven and self-motivated, and constantly experimenting with new technologies and techniques. I am passionate about Web Development and strive to better myself as a developer, and the community as a whole.',
    email: 'mailto:chriswiles87@gmail.com',
    resume: 'https://drive.google.com/open?id=1wJ_zFvz669sdqUCjNdELGPNjHX4pZ_NMPIEuYgZ1mok'
